3134ea3544e6c5115e41b734c44ff06f761ce2d8
deffname <juyuhang@126.com>
2024-04-02 11:56:06 +0800

    建议项目

---
+++ src/main/java/com/example/jmgexamsys03/JmgExamSys03Application.java
@@ -0,0 +1,15 @@
+package com.example.jmgexamsys03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmgExamSys03Application {
+
+    public static void main(String[] args) {
+        System.out.println("success");
+        SpringApplication.run(JmgExamSys03Application.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/jmgexamsys03/JmgExamSys03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmgexamsys03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmgExamSys03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
