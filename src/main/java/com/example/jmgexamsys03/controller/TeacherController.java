f2def4d4e2c6f7a448620cf434738db4c85aac2c
gqy <gtug538@gmail.com>
2024-06-02 06:33:04 +0800

    教师接口请求

---
+++ src/main/java/com/example/jmgexamsys03/controller/TeacherController.java
@@ -0,0 +1,61 @@
+package com.example.jmgexamsys03.controller;
+
+
+import com.example.jmgexamsys03.domain.ResponseResult;
+import com.example.jmgexamsys03.entity.Dto.AddStudentDto;
+import com.example.jmgexamsys03.entity.Dto.ChangeExamDto;
+import com.example.jmgexamsys03.entity.Dto.CheckExamDto;
+import com.example.jmgexamsys03.entity.Dto.CreateExamDto;
+import com.example.jmgexamsys03.entity.Dto.DeleteStudentDto;
+import com.example.jmgexamsys03.entity.Dto.UploadExamDto;
+import com.example.jmgexamsys03.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/teacher")
+public class TeacherController {
+    private TeacherService teacherService;
+
+    @Autowired
+    public TeacherController(TeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+    @PostMapping("/exam/create")
+    public ResponseResult CreateExamController(@RequestBody CreateExamDto createExamDto){
+        return teacherService.CreateExam(createExamDto);
+    }
+
+    @PostMapping("/exam/changeInfo")
+    public ResponseResult ChangeExamController(@RequestBody ChangeExamDto changeExamDto){
+        return teacherService.ChangeExam(changeExamDto);
+    }
+
+    @PostMapping ("/exam/uploadPaper")
+    public ResponseResult UploadExamController(@RequestBody UploadExamDto uploadExamDto){
+        return teacherService.UploadExam(uploadExamDto);
+    }
+    @PostMapping("/exam/addStu")
+    public ResponseResult  AddStudentController(@RequestBody AddStudentDto addStudentDto){
+        return teacherService.AddStudent(addStudentDto);
+    }
+    @PostMapping("/exam/deleteStu")
+    public ResponseResult  DeleteStudentController(@RequestBody DeleteStudentDto deleteStudentDto){
+        return teacherService.DeleteStudent(deleteStudentDto);
+    }
+    @GetMapping("/exam/start")
+    public ResponseResult  StartExamController(){
+        return teacherService.StartExam();
+    }
+    @PostMapping("/examing/getInfo")
+    public ResponseResult  CheckExamController(@RequestBody CheckExamDto checkExamDto){
+        return teacherService.CheckExam(checkExamDto);
+    }
+    @GetMapping("/exam/end")
+    public ResponseResult  EndExamController(){
+        return teacherService.EndExam();
+    }
+}
+
+
